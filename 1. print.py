a24e0a1b697f0d8ef78e0b4bdbf5e9eb95ae19bd
jinw96 <jinw960@gmail.com>
2018-10-28 20:25:55 +0900

    '1. print' added

---
+++ 1. print.py
@@ -0,0 +1,40 @@
+# 1
+print("\n# 1")
+print("Life is short.")
+print("You need Python.")
+
+# 2
+print("\n# 2")
+a = 3
+b = 2
+print(a+b)
+
+# 3
+print("\n# 3")
+a = "Hello"
+b = "World"
+print(a+b)
+print(a,b)
+ 
+# 4
+print("\n# 4")
+a = 2018
+b = 11
+c = 1
+print("Year:", a)
+print("Month:", b)
+print("Date:", c)
+print()
+print(f"Year: {a}")
+print(f"Month: {b}")
+print(f"Date: {c}")
+
+# 5
+print("\n# 5")
+print("C\tE\tO\tS")
+
+# 6
+print("\n# 6")
+print("Alssong", end=' ')
+print("Dalssong", end=' ')
+print("Pythong")
